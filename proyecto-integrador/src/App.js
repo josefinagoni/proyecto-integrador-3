8ce4d6b0d5371ae001e14650a91ceb2447f69bcc
josefinagoni <gonij@udesa.edu.ar>
2021-09-06 12:04:34 -0300

    app con movies de api

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
-<<<<<<< HEAD
 import Movies from './components/Movies/Movies'
-=======
-import Card from './components/Card/Card';
 import Footer from './components/Footer/Footer'
->>>>>>> 97ff5a8a5829c6a4bfe4233e88266e98a82118fa
 
 
 function App() {
